521d5a8c149c9eca40a4ffef8e4907dd3733a188
Mirco <mirco@mircos-air.homenet.telecomitalia.it>
2019-03-24 01:25:20 +0100

    Updates

---
+++ precache-manifest.d262fce334dc911beb1ba425bd33efe1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0cd9058418b79059a16",
+    "url": "/react-tmdb-player/static/js/runtime~main.38002364.js"
+  },
+  {
+    "revision": "80f2b39145b2721c8108",
+    "url": "/react-tmdb-player/static/js/main.dc7eb568.chunk.js"
+  },
+  {
+    "revision": "6e3977e2310da164dbc7",
+    "url": "/react-tmdb-player/static/js/2.a630c545.chunk.js"
+  },
+  {
+    "revision": "80f2b39145b2721c8108",
+    "url": "/react-tmdb-player/static/css/main.c993529a.chunk.css"
+  },
+  {
+    "revision": "6e3977e2310da164dbc7",
+    "url": "/react-tmdb-player/static/css/2.893dd009.chunk.css"
+  },
+  {
+    "revision": "9cae6c7c45d360e3a761ec74072a2dab",
+    "url": "/react-tmdb-player/index.html"
+  }
+];
