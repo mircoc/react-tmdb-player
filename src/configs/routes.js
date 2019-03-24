8d77fd193eb8e529f416cd1d94a2e6f3c19d7435
Mirco <mirco@mircos-air.homenet.telecomitalia.it>
2019-03-24 01:31:03 +0100

    routes for deploy

--- src/configs/routes.js
+++ src/configs/routes.js
@@ -1,20 +1,21 @@
 import { ITEM_TYPES } from "../apis";
+const BASEPATH = `/react-tmdb-player`
 
 const routes = {
-  HOME: '/',
-  DETAILMOVIE: '/movie/:id',
-  DETAILTV: '/tv/:id',
-  SEARCH: '/search/',
-  SEARCHKEYWORD: '/search/:keyword',
+  HOME: `${BASEPATH}/`,
+  DETAILMOVIE: `${BASEPATH}/movie/:id`,
+  DETAILTV: `${BASEPATH}/tv/:id`,
+  SEARCH: `${BASEPATH}/search/`,
+  SEARCHKEYWORD: `${BASEPATH}/search/:keyword`,
 }
 
 export const getRouteForDetail = (id, itemType) => {
   switch (itemType) {
     case ITEM_TYPES.TV:
-      return routes.DETAILTV.replace(':id', id)
+      return routes.DETAILTV.replace(`:id`, id)
     case ITEM_TYPES.MOVIE:
     default:
-      return routes.DETAILMOVIE.replace(':id', id)
+      return routes.DETAILMOVIE.replace(`:id`, id)
     
   }
 }
